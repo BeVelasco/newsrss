4b7878a27f54bf3c4bdf8a5b2e5359d9794cacce
BeVelasco <45297332+BeVelasco@users.noreply.github.com>
2021-02-17 17:33:42 -0600

    GBR Actualizacion
    GBR Actualizacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Robot Guillermo berruecos Rodríguez</title>
+    <title>Robot Guillermo Berruecos Rodríguez</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
